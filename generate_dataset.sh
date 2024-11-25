849c06e9bcfdc1e96cd8b8a67701122450979b2b
Oli Bridge <ob344@cam.ac.uk>
2024-11-25 11:36:57 +0000

    Update bash script

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -1,17 +1,17 @@
 # Generate the train, test and val splits
 # python data_wrangling/split_data.py [input_file] [output_dir] -c [config_file]
 echo "<<Generating dataset splits...>>"
-python data_wrangling/split_data.py data2/master_labels.txt data2 -c config/data_config.yml
+python data_wrangling/split_data.py data/master_labels.txt data -c config/data_config.yml
 
 # Render images
 # python data_wrangling/render_data.py [input_file] [output_dir] -c [config_file]
 echo "<<Rendering images for each split...>>"
 
 echo "<<Rendering train split...>>"
-python data_wrangling/render_data.py data2/train -c config/data_config.yml
+python data_wrangling/render_data.py data/train -c config/data_config.yml
 
 echo "<<Rendering test split...>>"
-python data_wrangling/render_data.py data2/test -c config/data_config.yml
+python data_wrangling/render_data.py data/test -c config/data_config.yml
 
 echo "<<Rendering val split...>>"
-python data_wrangling/render_data.py data2/val -c config/data_config.yml
+python data_wrangling/render_data.py data/val -c config/data_config.yml
